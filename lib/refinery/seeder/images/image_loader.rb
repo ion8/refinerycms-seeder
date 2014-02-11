765e1f28c1276753d4bc3ead3e328ab139ff7e82
Vaz Allen <vaz@tryptid.com>
2014-02-11 02:49:34 -0400

    loading image paths

--- lib/refinery/seeder/images/image_loader.rb
+++ lib/refinery/seeder/images/image_loader.rb
@@ -1,8 +1,23 @@
 module Refinery::Seeder
   module Images
+    class ImageLoadError < IOError; end
+
     class ImageLoader
       def images_root
         File.join(Refinery::Seeder.resources_root, 'images')
+      end
+
+      def collect_image_paths
+        unless File.directory?(images_root)
+          raise ImageLoadError.new(
+            "images root #{images_root} is not a directory")
+        end
+
+        Dir[File.join(images_root, '**/*')].select do |file|
+          File.file?(file) && File.readable?(file) && (
+            file =~ /\.(png|jpe?g|gif)$/i
+          )
+        end
       end
     end
   end
--- spec/refinery/seeder/images_spec.rb
+++ spec/refinery/seeder/images_spec.rb
@@ -10,4 +10,18 @@ describe Refinery::Seeder::Images::ImageLoader do
     subject.images_root.should start_with Refinery::Seeder.resources_root
     subject.images_root.should end_with 'images'
   end
+
+  context "loads images" do
+    before :each do
+      allow(Refinery::Seeder).to receive(:resources_root).and_return File.join(
+        File.expand_path('../../..', __FILE__), # spec/
+        'resources'
+      )
+    end
+
+    it "collects paths to images" do
+      paths = subject.collect_image_paths
+      paths.length.should == 3
+    end
+  end
 end
