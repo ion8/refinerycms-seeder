86b77598f6fa1fe8e01f5160c9a01f80df3f56d4
Vaz Allen <vaz@tryptid.com>
2014-02-14 01:13:31 -0400

    Fix reference to I18n module

--- lib/refinery/seeder/ext.rb
+++ lib/refinery/seeder/ext.rb
@@ -1,13 +1,13 @@
 require 'i18n'
 
 
-I18n.enforce_available_locales = false
+::I18n.enforce_available_locales = false
 
 module Refinery
   module Seeder
     module StringExtensions
       def underscored_word
-        I18n.transliterate(self).
+        ::I18n.transliterate(self).
         gsub(/([a-z])([A-Z])/, '\1_\2').
         gsub(/([A-Z])([A-Z][a-z])/, '\1_\2').
         gsub(/\s+/, '_').
